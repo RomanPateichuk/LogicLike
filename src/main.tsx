c97a0cb4ff41c29260862028bad9b6c42c9aab97
Roman Patsiaichuk <roman.shady@yandex.ru>
2024-05-26 20:25:08 +0300

    add deploy workflow

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css'
 import {Provider} from "react-redux";
 import {store} from "./redux/configure-store.ts";
 import {AppRoutes} from "./routes/appRoutes.tsx";
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <BrowserRouter>
         <AppRoutes/>
-      </Router>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
