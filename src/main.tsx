50128302c753dc6d0522899592513998809ea71b
Roman Patsiaichuk <roman.shady@yandex.ru>
2024-05-26 20:49:51 +0300

    add deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {HashRouter} from 'react-router-dom'
 import './index.css'
 import {Provider} from "react-redux";
 import {store} from "./redux/configure-store.ts";
 import {AppRoutes} from "./routes/appRoutes.tsx";
-import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <AppRoutes/>
-      </BrowserRouter>
+      <HashRouter>
+ <AppRoutes/>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
